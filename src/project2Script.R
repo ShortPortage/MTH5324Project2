f31b9d9520f11734d1c3f57a160ec9e902935725
Patrick Quinn <pquinn2019@my.fit.edu>
2026-04-13 23:46:13 -0400

    did the other useful/not useful selection for data

--- src/project2Script.R
+++ src/project2Script.R
@@ -1,38 +1,41 @@
 library(archive)
 library(readr)
 
+# Responses: Fatal accident indicator, number of vehicles
+# Predictors: Speed, weather, lighting, road type, time, driver age, alcohol, location
+
 # Load .csv files
 zip = "./data/data.zip"
-accident = read_csv(archive_read(zip, file = "FARS2022NationalCSV/accident.csv"))# useful  
-cevent = read_csv(archive_read(zip, file = "FARS2022NationalCSV/cevent.csv"))
-crashrf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/crashrf.csv")) #not useful
-damage = read_csv(archive_read(zip, file = "FARS2022NationalCSV/damage.csv"))
-distract = read_csv(archive_read(zip, file = "FARS2022NationalCSV/distract.csv"))
-drimpair = read_csv(archive_read(zip, file = "FARS2022NationalCSV/drimpair.csv"))
-driverrf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/driverrf.csv")) #useful - indicates the drivers fault 
-drugs = read_csv(archive_read(zip, file = "FARS2022NationalCSV/drugs.csv")) #useful
-factor = read_csv(archive_read(zip, file = "FARS2022NationalCSV/factor.csv"))
-maneuver = read_csv(archive_read(zip, file = "FARS2022NationalCSV/maneuver.csv"))
-miacc = read_csv(archive_read(zip, file = "FARS2022NationalCSV/MIACC.CSV"))
-midrvacc = read_csv(archive_read(zip, file = "FARS2022NationalCSV/MIDRVACC.CSV"))
-miper = read_csv(archive_read(zip, file = "FARS2022NationalCSV/MIPER.CSV"))
-nmcrash = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmcrash.csv"))
-nmdistract = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmdistract.csv"))
-nmimpair = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmimpair.csv"))
-nmprior = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmprior.csv"))
-parkwork = read_csv(archive_read(zip, file = "FARS2022NationalCSV/parkwork.csv")) #useful - indicates time of the incident and more info 
-pbtype = read_csv(archive_read(zip, file = "FARS2022NationalCSV/pbtype.csv"))   # useful indicates age
-person = read_csv(archive_read(zip, file = "FARS2022NationalCSV/person.csv"))  # race of person (white, black,etc)
-personrf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/personrf.csv"))
-pvehiclesf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/pvehiclesf.csv"))
-race = read_csv(archive_read(zip, file = "FARS2022NationalCSV/race.csv"))
-safetyeq = read_csv(archive_read(zip, file = "FARS2022NationalCSV/safetyeq.csv"))
-vehicle = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vehicle.csv"))# car type not sure if useful
-vehiclesf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vehiclesf.csv"))
-vevent = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vevent.csv"))
-violatn = read_csv(archive_read(zip, file = "FARS2022NationalCSV/violatn.csv"))
+accident = read_csv(archive_read(zip, file = "FARS2022NationalCSV/accident.csv")) # useful  
+#cevent = read_csv(archive_read(zip, file = "FARS2022NationalCSV/cevent.csv"))     # not useful, lists how it happened
+#crashrf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/crashrf.csv"))   # not useful
+damage = read_csv(archive_read(zip, file = "FARS2022NationalCSV/damage.csv"))     # useful, gives number of vehicles
+#distract = read_csv(archive_read(zip, file = "FARS2022NationalCSV/distract.csv")) # not useful, gives if distracted driver
+drimpair = read_csv(archive_read(zip, file = "FARS2022NationalCSV/drimpair.csv")) # useful, alcohol/drugs
+driverrf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/driverrf.csv")) # useful - indicates the drivers fault 
+drugs = read_csv(archive_read(zip, file = "FARS2022NationalCSV/drugs.csv"))       # useful, gives drug info
+#factor = read_csv(archive_read(zip, file = "FARS2022NationalCSV/factor.csv"))     # not useful
+#maneuver = read_csv(archive_read(zip, file = "FARS2022NationalCSV/maneuver.csv")) # not useful, gives if driver tried to avoid
+#miacc = read_csv(archive_read(zip, file = "FARS2022NationalCSV/MIACC.CSV"))       # not useful
+#midrvacc = read_csv(archive_read(zip, file = "FARS2022NationalCSV/MIDRVACC.CSV")) # not useful
+#miper = read_csv(archive_read(zip, file = "FARS2022NationalCSV/MIPER.CSV"))       # not useful
+#nmcrash = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmcrash.csv"))   # not useful, gives reason for crash
+nmdistract = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmdistract.csv"))  # useful, gives if distracted in a simpler way
+nmimpair = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmimpair.csv")) # useful, gives if impaired
+#nmprior = read_csv(archive_read(zip, file = "FARS2022NationalCSV/nmprior.csv"))   # not useful
+parkwork = read_csv(archive_read(zip, file = "FARS2022NationalCSV/parkwork.csv")) # useful - indicates time of the incident and more info 
+pbtype = read_csv(archive_read(zip, file = "FARS2022NationalCSV/pbtype.csv"))     # useful indicates age
+person = read_csv(archive_read(zip, file = "FARS2022NationalCSV/person.csv"))     # useful, age, gender, race of person (white, black, etc) - all sorts of stuff
+#personrf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/personrf.csv")) # not useful
+#pvehiclesf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/pvehiclesf.csv")) # not useful
+race = read_csv(archive_read(zip, file = "FARS2022NationalCSV/race.csv"))         # also gives race
+#safetyeq = read_csv(archive_read(zip, file = "FARS2022NationalCSV/safetyeq.csv")) # not useful, gives safety equipment
+vehicle = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vehicle.csv"))   # useful
+#vehiclesf = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vehiclesf.csv")) # not useful
+#vevent = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vevent.csv"))     # not useful
+#violatn = read_csv(archive_read(zip, file = "FARS2022NationalCSV/violatn.csv"))   # not useful
 vision = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vision.csv"))# vision interference  useful
-vpicdecode = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vpicdecode.csv"))
-vpictrailer = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vpictrailerdecode.csv"))
-vsoe = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vsoe.csv"))
+vpicdecode = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vpicdecode.csv")) # maybe useful, gives vehicle info
+#vpictrailer = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vpictrailerdecode.csv")) # not useful
+#vsoe = read_csv(archive_read(zip, file = "FARS2022NationalCSV/vsoe.csv"))  # not useful
 weather = read_csv(archive_read(zip, file = "FARS2022NationalCSV/weather.csv"))  #useful
